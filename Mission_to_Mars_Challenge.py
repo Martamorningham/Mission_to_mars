69d1068439a395c97f2f618ffd57a4945edf36f1
Martamorningham <matthewgoldstein90@gmail.com>
2022-09-05 13:57:12 -0600

    comit

--- Mission_to_Mars_Challenge.py
+++ Mission_to_Mars_Challenge.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-Import Splinter, BeautifulSoup, and Pandas
+#Import Splinter, BeautifulSoup, and Pandas
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 import pandas as pd
